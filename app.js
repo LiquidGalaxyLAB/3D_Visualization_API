f4fcef260e9a5aba2e577dd3c211a25c4c8415db
Sara Caballero Bruno <sara2cb@gmail.com>
2020-06-02 13:52:25 +0100

    Camera angle in browsers
    The angle of the new camera has been adapted for each browser, and
    also the angle would update every time a new browser is introduced

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-var app = require('express')();
+const express = require('express')
+const app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
 });
 
-users = [];
-noUsers = 0;
-activeUser = 1;
+var noUsers = 0;
+var activeUser = 1;
+var angleNext = 0;
+var lookingRight = 1; 
 io.on('connection', function(socket) {
    noUsers++;
    var id = noUsers;
    console.log('Screen number ' + noUsers + ' connected');
-   socket.emit('idSet', {id: noUsers, active: noUsers==activeUser});
+   socket.emit('idSet', {id: noUsers, active: noUsers==activeUser, angle: angleNext*lookingRight});
+   if(noUsers%2 ==1){ angleNext+=45; }
+   lookingRight = -lookingRight;
+  
+   if(noUsers>2){
+      io.sockets.emit('whatTime');
+   }
 
-   socket.on('moveObject', function(data) {
-     activeUser++;
-     if(activeUser>noUsers) activeUser = 1;
-    //Send message to everyone
-    io.sockets.emit('nextScreen', activeUser);
- })
- socket.on('disconnect', (reason) => {
-    console.log('Screen number ' + noUsers + ' disconnected');
-    noUsers--;
-    if(activeUser>noUsers) activeUser = 1;
-    io.sockets.emit('oneLessScreen', {noUser: id, active: activeUser});
-  });
+   socket.on('currentTime', function(data) {
+      io.sockets.emit('setCube', data);
+   })
+
+   socket.on('updateID', function(data) {
+      if(data > noUsers){id--;}
+   })
+
+   socket.on('disconnect', (reason) => {
+      console.log('Screen number ' + id + ' disconnected');
+      noUsers--;
+      if(noUsers%2 ==0){ angleNext-=45; }
+      io.sockets.emit('oneLessScreen', {noUser: id, noUsers: noUsers});
+      io.sockets.emit('updateID', id);
+      io.sockets.emit('whatTime');
+   });
 });
 
 http.listen(3000, function() {
---
+++ public/main.js
@@ -0,0 +1,73 @@
+if(rotateCamera == null){
+    rotateCamera = false
+}if(angleCamera == null){
+    angleCamera = false
+}
+const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const height = window.innerHeight|| document.documentElement.clientHeight||  document.body.clientHeight;
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var camera_pivot = new THREE.Object3D()
+var Y_AXIS = new THREE.Vector3( 0, 1, 0 );
+
+scene.add( camera );
+camera.position.z = 5;
+
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+var position
+var copyCamera= camera.clone();
+position = cube.position.clone();
+position.project(camera);
+window.addEventListener( 'resize', onWindowResize, false );
+cube.position.x = 0;
+var angleBef = 0;
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    if(rotateCamera){
+        camera.rotateOnAxis( Y_AXIS, degreesToRadians(-angleBef) );
+        camera.rotateOnAxis( Y_AXIS, degreesToRadians(angleCamera) );
+        rotateCamera = false;
+        angleBef = angleCamera
+    }
+    cube.position.x += 0.05;
+    position = cube.position.clone();
+    position.project(camera);
+    if(cube.position.x>8){
+        cube.position.x = -cube.position.x;
+    }
+    
+    renderer.render( scene, camera );
+};
+
+animate();
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function degreesToRadians(degrees){
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+function getCubePosition(){
+    return cube.position;
+}
+function setCubePosition(position){
+    cube.position.x = position.x;
+    cube.position.y = position.y;
+    cube.position.z = position.y;
+}
