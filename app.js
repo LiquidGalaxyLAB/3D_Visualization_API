cb3254a329f8858a23158aa17b1274cc48e00527
Sara Caballero Bruno <sara2cb@gmail.com>
2020-06-03 20:25:46 +0100

    Adding all cases of browser closure
    In this code we specify 3 possible actions that the system might taking when closing a socket. Mirroring the browsers so that they will be added to the left instead of the right. Moving one, when a socket we close and the system simply swtches one closer to the center. And reorganising which is when one side has way more sockets that the other, in which case the center needs to change.

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ var noUsers = 0;
 var activeUser = 1;
 var angleNext = 0;
 var lookingRight = 1; 
+var startRight = true;
 io.on('connection', function(socket) {
    noUsers++;
    var id = noUsers;
    console.log('Screen number ' + noUsers + ' connected');
    socket.emit('idSet', {id: noUsers, active: noUsers==activeUser, angle: angleNext*lookingRight});
-   if(noUsers%2 ==1){ angleNext+=45; }
+   if(noUsers%2 == 1){ angleNext+=45; }
    lookingRight = -lookingRight;
   
    if(noUsers>2){
@@ -30,16 +31,66 @@ io.on('connection', function(socket) {
       io.sockets.emit('setCube', data);
    })
 
-   socket.on('updateID', function(data) {
-      if(data > noUsers){id--;}
+   socket.on('updateIDReorganise', function(data) {
+      console.log("reorganise " + id)
+      if(id==2){id = 1;}
+      else if(id%2 == 0){
+         id = id-2;
+      }else{
+         if(data > id){
+            id += 2;
+         }
+      }
+      console.log("reorganise " + id)
+   })
+
+   socket.on('updateIDMove', function(data) {
+      console.log("move " + id)
+      if(data%2 == id%2 && id > data){id -=2;}
+      console.log("move " + id)
+   })
+
+   socket.on('updateIDMirror', function() {
+      console.log("mirror " + id)
+      if(id > 1){
+         if(id%2==1){
+            id--;
+         }else{
+            id++;
+         }
+      }
+      console.log("mirror " + id)
    })
 
    socket.on('disconnect', (reason) => {
       console.log('Screen number ' + id + ' disconnected');
+      console.log(noUsers + " " + angleNext + " " + lookingRight);
       noUsers--;
-      if(noUsers%2 ==0){ angleNext-=45; }
-      io.sockets.emit('oneLessScreen', {noUser: id, noUsers: noUsers});
-      io.sockets.emit('updateID', id);
+      if(noUsers%2 ==0){ 
+         angleNext-=45; 
+         if(id%2 == 1){
+            lookingRight = -lookingRight;
+         }
+      }else{
+         lookingRight = -lookingRight;
+      }
+      console.log(noUsers + " " + angleNext + " " + lookingRight);
+
+      if(noUsers %2 == 1 && id%2 ==1){
+         io.sockets.emit('updateIDReorganise', id);
+         io.sockets.emit('updateIDReorganiseSock', {noUser:id, startRight:startRight});
+      }else{
+         io.sockets.emit('updateIDMove', id);
+         io.sockets.emit('updateIDMoveSock', id);
+
+         if(id%2==0 && noUsers%2==0){
+            io.sockets.emit('updateIDMirror');
+            io.sockets.emit('updateIDMirrorSock');
+            startRight = !startRight
+         }
+      
+      }
+
       io.sockets.emit('whatTime');
    });
 });
